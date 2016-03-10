69e47fd2272f4519ca086ad73351474ad1d1cab3
fatihozel <fthsec@gmail.com>
2016-03-10 18:53:00 +0200

    Update nsa.py

--- nsa.py
+++ nsa.py
@@ -1,3 +1,5 @@
 from scapy.all import *
 
+ans, unans = sr(IP(dst="10.99.5.22")/TCP(sport=4444, dport=[135, 445, 8080, 443, 80, 1433], flags="S"))
 
+ans.summary(lambda s,r: r.sprintf("TCP: {0} %TCP.sport% ----------------> %TCP.flags%".format(str(r[TCP].sport))))
